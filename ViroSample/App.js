bca73ee7b913a1e3a7419721e6fe2c9875922c97
Olivia Sztanga <oliviasztanga@gmail.com>
2019-11-25 11:43:24 -0500

    feat: added local state-based navigation set up

--- ViroSample/App.js
+++ ViroSample/App.js
@@ -6,7 +6,6 @@
  * LICENSE file in the root directory of this source tree. An additional grant
  * of patent rights can be found in the PATENTS file in the same directory.
  */
-// commit
 
 import React, { Component } from 'react'
 import {
@@ -24,6 +23,7 @@ import {
   ViroVRSceneNavigator,
   ViroARSceneNavigator
 } from 'react-viro';
+import { Explorer } from './js/Explorer';
 
 /*
  TODO: Insert your API key below
@@ -36,103 +36,133 @@ var sharedProps = {
 var InitialARScene = require('./js/Explorer');
 
 
-var UNSET = "UNSET";
-var EXPLORE_NAVIGATOR_TYPE = "Explore Exhibitions";
-var COLLECTION_NAVIGATOR_TYPE = "View our Collection";
+// var UNSET = "UNSET";
+// var EXPLORE_NAVIGATOR_TYPE = "Explore Exhibitions";
+// var COLLECTION_NAVIGATOR_TYPE = "View our Collection";
 
 // This determines which type of experience to launch in, or UNSET, if the user should
 // be presented with a choice of AR or VR. By default, we offer the user a choice.
 var defaultNavigatorType = UNSET;
+
+// NAVIGATION HELPER VARIABLES
+const HOME = 'HOME'
+const MAP = 'MAP'
+const EXPLORER = 'EXPLORER'
+const EXHIBITIONS = 'EXHIBTIONS'
 
 export default class App extends Component {
   constructor() {
     super();
 
     this.state = {
-      navigatorType : defaultNavigatorType,
-      sharedProps : sharedProps
+      currentPage: HOME
+      // navigatorType : defaultNavigatorType,
+      // sharedProps : sharedProps
     }
-    this._getExperienceSelector = this._getExperienceSelector.bind(this);
-    this._getExploreNavigator = this._getExploreNavigator.bind(this);
-    this._getCollectionNavigator = this._getCollectionNavigator.bind(this);
-    this._getExperienceButtonOnPress = this._getExperienceButtonOnPress.bind(this);
-    this._exitViro = this._exitViro.bind(this);
+
+    // this._getExperienceSelector = this._getExperienceSelector.bind(this);
+    // this._getExploreNavigator = this._getExploreNavigator.bind(this);
+    // this._getCollectionNavigator = this._getCollectionNavigator.bind(this);
+    // this._getExperienceButtonOnPress = this._getExperienceButtonOnPress.bind(this);
+    // this._exitViro = this._exitViro.bind(this);
   }
 
   render() {
-    if (this.state.navigatorType == UNSET) {
-      return this._getExperienceSelector();
-    } else if (this.state.navigatorType == COLLECTION_NAVIGATOR_TYPE) {
-      return this._getCollectionNavigator();
-    } else if (this.state.navigatorType == EXPLORE_NAVIGATOR_TYPE) {
-      return this._getExploreNavigator();
+    let currentView = <Home />
+    switch (this.state.currentPage) {
+      case HOME:
+        currentView = <Home />
+      case MAP:
+        currentView = <MapScreeen />
+      case EXPLORER:
+        currentView = <Explorer />
+      case EXHIBITIONS:
+        currentView = <Exhibitions />
+      default:
+        currentView = <Home />
     }
+
+    return (
+      <view>
+        <Header />
+        {currentView}
+        <Footer />
+      </view>
+    )
+
+    // if (this.state.navigatorType == UNSET) {
+    //   return this._getExperienceSelector();
+    // } else if (this.state.navigatorType == COLLECTION_NAVIGATOR_TYPE) {
+    //   return this._getCollectionNavigator();
+    // } else if (this.state.navigatorType == EXPLORE_NAVIGATOR_TYPE) {
+    //   return this._getExploreNavigator();
+    // }
   }
 
   // Presents the user with a choice of an AR experiences
-  _getExperienceSelector() {
-    return (
-      <View style={localStyles.viroContainer}>
-          <View style={localStyles.backgroundContainer}>
+  // _getExperienceSelector() {
+  //   return (
+  //     <View style={localStyles.viroContainer}>
+  //         <View style={localStyles.backgroundContainer}>
 
-          <Text style={localStyles.titleText}>muralARts</Text>
+  //         <Text style={localStyles.titleText}>muralARts</Text>
 
-          <ImageBackground source={require('./js/res/collection/biggie.jpg')} style={localStyles.backgroundImage} style={{width: '100%', height: '100%'}} style = {localStyles.backdrop}/>
+  //         <ImageBackground source={require('./js/res/collection/biggie.jpg')} style={localStyles.backgroundImage} style={{width: '100%', height: '100%'}} style = {localStyles.backdrop}/>
 
-          <Text style={localStyles.catchphraseText}>
-          re imagining the space we’re in with unbound creativity
-          </Text>
+  //         <Text style={localStyles.catchphraseText}>
+  //         re imagining the space we’re in with unbound creativity
+  //         </Text>
 
-          <TouchableHighlight style={localStyles.buttons}
-            onPress={this._getExperienceButtonOnPress(EXPLORE_NAVIGATOR_TYPE)}
-            underlayColor={'#d3d3d3'} >
+  //         <TouchableHighlight style={localStyles.buttons}
+  //           onPress={this._getExperienceButtonOnPress(EXPLORE_NAVIGATOR_TYPE)}
+  //           underlayColor={'#d3d3d3'} >
 
-            <Text style={localStyles.buttonText}>Explore Exhibitions  ></Text>
-          </TouchableHighlight>
+  //           <Text style={localStyles.buttonText}>Explore Exhibitions  ></Text>
+  //         </TouchableHighlight>
 
-          <TouchableHighlight style={localStyles.buttons}
-            onPress={this._getExperienceButtonOnPress(COLLECTION_NAVIGATOR_TYPE)}
-            underlayColor={'#d3d3d3'} >
+  //         <TouchableHighlight style={localStyles.buttons}
+  //           onPress={this._getExperienceButtonOnPress(COLLECTION_NAVIGATOR_TYPE)}
+  //           underlayColor={'#d3d3d3'} >
 
-            <Text style={localStyles.buttonText}>View our Collection  ></Text>
-          </TouchableHighlight>
-          </View>
-      </View>
-    );
-  }
+  //           <Text style={localStyles.buttonText}>View our Collection  ></Text>
+  //         </TouchableHighlight>
+  //         </View>
+  //     </View>
+  //   );
+  // }
 
   // Returns the ViroARSceneNavigator which will start the AR experience
-  _getExploreNavigator() {
-    return (
-      <ViroARSceneNavigator {...this.state.sharedProps}
-        initialScene={{scene: InitialARScene}} />
-    );
-  }
+  // _getExploreNavigator() {
+  //   return (
+  //     <ViroARSceneNavigator {...this.state.sharedProps}
+  //       initialScene={{scene: InitialARScene}} />
+  //   );
+  // }
 
   // Returns the ViroSceneNavigator which will start the VR experience
-  _getCollectionNavigator() {
-    return (
-      <ViroVRSceneNavigator {...this.state.sharedProps}
-        initialScene={{scene: InitialVRScene}} onExitViro={this._exitViro}/>
-    );
-  }
+  // _getCollectionNavigator() {
+  //   return (
+  //     <ViroVRSceneNavigator {...this.state.sharedProps}
+  //       initialScene={{scene: InitialVRScene}} onExitViro={this._exitViro}/>
+  //   );
+  // }
 
   // This function returns an anonymous/lambda function to be used
   // by the experience selector buttons
-  _getExperienceButtonOnPress(navigatorType) {
-    return () => {
-      this.setState({
-        navigatorType : navigatorType
-      })
-    }
-  }
+  // _getExperienceButtonOnPress(navigatorType) {
+  //   return () => {
+  //     this.setState({
+  //       navigatorType : navigatorType
+  //     })
+  //   }
+  // }
 
   // This function "exits" Viro by setting the navigatorType to UNSET.
-  _exitViro() {
-    this.setState({
-      navigatorType : UNSET
-    })
-  }
+  // _exitViro() {
+  //   this.setState({
+  //     navigatorType : UNSET
+    // })
+  // }
 }
 
 var localStyles = StyleSheet.create({
